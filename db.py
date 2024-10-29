c1e6273782f8819d280a7940db9197ab3310471b
YuitoAkatsuki <yuito@yuito.work>
2024-10-30 06:34:10 +0900

    ✨ Update database configuration from MySQL to PostgreSQL in deployment and environment files

--- db.py
+++ db.py
@@ -30,7 +30,7 @@ class Subscribe(Base):
 # engine = create_engine(
 #     f'postgresql://twibot:{dbPassword}@db/subscribeList', echo=True)
 engine = create_engine(
-    f'mysql://twibot:{dbPassword}@db/slack_twitter', echo=True)
+    f'postgresql://twibot:{dbPassword}@db/twibot', echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # 4 dev
